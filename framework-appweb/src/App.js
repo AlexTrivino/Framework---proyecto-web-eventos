0c987d67e4eb803ee08a5d9989af08bcf1cd8fb6
Yuli200431 <yuleisi.cursos2004@gmail.com>
2025-07-31 21:45:23 -0500

    La dirrecion en Index estaban mal, decian componen

--- framework-appweb/src/App.js
+++ framework-appweb/src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import './styles/index.css';
-import Login from './components/Login';
-import Index from './components/Index';
-import CrearEvento from './components/CrearEvento';
-import RegistrarseEventos from './components/RegistrarseEventos';
+import Login from './pages/Login';
+import Index from './pages/index';
+import CrearEvento from './pages/CrearEvento';
+import RegistrarseEventos from './pages/RegistrarseEventos';
 
 function App() {
   return (
