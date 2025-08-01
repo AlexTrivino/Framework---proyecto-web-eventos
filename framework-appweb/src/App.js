24722b2504d9dfd2d7166fcf4a2bdc7067610b1d
Yuli200431 <yuleisi.cursos2004@gmail.com>
2025-07-31 20:54:15 -0500

    Commit del Login y RegistraseEventos

--- framework-appweb/src/App.js
+++ framework-appweb/src/App.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import Index from './pages/index.jsx';
-import CrearEvento from './pages/crearEvento.jsx';
+import './styles/index.css';
+import Login from './components/Login';
+import Index from './components/Index';
+import CrearEvento from './components/CrearEvento';
+import RegistrarseEventos from './components/RegistrarseEventos';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Navigate to="/login" replace />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/index" element={<Index />} />
         <Route path="/crearEvento" element={<CrearEvento />} />
+        <Route path="/registrarseEventos" element={<RegistrarseEventos />} />
       </Routes>
     </Router>
   );
